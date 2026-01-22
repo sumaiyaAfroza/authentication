4865df3c2bf1c54cf60e5e93721b03d0c50413ad
NSC <sumaiya.afroza.99@gmail.com>
2026-01-22 12:08:34 +0600

    controllers

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,3 +1,25 @@
 import asyncHandler from "express-async-handler";
+import {User} from "../models/user.model.js";
 
-const registerUser = asyncHandler(ay)
+const registerUser = asyncHandler(async (req,res) => {
+  const {name, email, password} = req.body
+  const  userExists = await  User.findOne({email})
+  if(userExists) {
+    res.status(400)
+    throw new Error('user already exists')
+  }
+  const user = await User.create({
+    name,email,password
+  })
+  if(user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email
+    })
+  }
+  else {
+    res.status(400)
+    throw  new Error('invalid user data')
+  }
+})
