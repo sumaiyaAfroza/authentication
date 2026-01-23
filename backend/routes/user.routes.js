00fd906b7dc10148b7fd1fc77b42b0bf999594b5
NSC <sumaiya.afroza.99@gmail.com>
2026-01-23 12:28:18 +0600

    profile create & token, cookies parser

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import {authUser, logout, registerUser} from "../controllers/user.controllers.js";
+import {authUser, getUserProfile, logout, registerUser} from "../controllers/user.controllers.js";
+import {protect} from "../middleware/auth.middleware.js";
 
 export  const router = express.Router()
 
 router.post('/register', registerUser)
 router.post('/login', authUser)
 router.post('/logout', logout)
+router.route('/profile')
+      .get(protect,getUserProfile)
