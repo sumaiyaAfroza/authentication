324799a6b7918acf4066d13fc96760eed6545127
NSC <sumaiya.afroza.99@gmail.com>
2026-01-23 12:28:38 +0600

    profile create & token, cookies parser

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import {connectDB} from "./config/db.js";
 import cors from 'cors'
 import {router as user} from "./routes/user.routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,6 +13,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+app.use(cookieParser())
 
 app.get('/', (req,res) => {
  res.send('server ready')
