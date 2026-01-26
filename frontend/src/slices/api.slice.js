409c00a83527b31415f8fa838ae3f3e4cc376d70
NSC <sumaiya.afroza.99@gmail.com>
2026-01-26 21:29:02 +0600

    apiSlice create

--- frontend/src/slices/api.slice.js
+++ frontend/src/slices/api.slice.js
@@ -1,5 +1,26 @@
-import {createApi} from "@reduxjs/toolkit/query/react";
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: import.meta.env.VITE_API,
+  credentials: "include",
+  prepareHeaders:(headers) => {
+    if(!headers.has('Content-Type')) {
+      headers.set('Content-Type' , 'application/json')
+    }
+    return headers
+  }
+})
+
+const baseQueryWithError = async (args, api , extraOptions ) => {
+  const result = await baseQuery(args,api,extraOptions)
+  if(result?.error?.status === 401 ) {
+    console.warn('unauthorized api')
+  }
+  return result
+}
 
 export const apiSlice = createApi({
+  baseQuery : baseQueryWithError,
+  tagTypes: ['user']
 
 })
