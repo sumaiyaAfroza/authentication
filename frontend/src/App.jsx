83c3cd2139d6cbbbf595631a99226a40d2a59544
sumaiyaAfroza <sumaiya.afroza.99@gmail.com>
2026-01-29 21:09:09 +0600

    logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import {Outlet} from "react-router";
+import Header from "./component/Header.jsx";
 
 const App = () => {
   return (
     <div>
-      {/*<Header/>*/}
+      <Header/>
       <Outlet/>
       
     </div>
--- frontend/src/slices/user.api.slice.js
+++ frontend/src/slices/user.api.slice.js
@@ -3,6 +3,8 @@ import {apiSlice} from "./api.slice.js";
  const userUrl = '/api/user'
 export const userApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
+
+    //  register
     register: builder.mutation({
       query: data => ({
         url: `${userUrl}/register`,
@@ -10,9 +12,18 @@ export const userApiSlice = apiSlice.injectEndpoints({
         body: data
       }),
       invalidatesTags: ['user']
-    })
-  })
+    }),
+
+   //  logout user
+   logout: builder.mutation({
+    query: () => ({
+      url: `${userUrl}/logout`,
+      method: "POST"
+    }),
+    invalidatesTags: ['user']
+   })
+ })
 })
 
 
-export const {useRegisterMutation} = userApiSlice
+export const {useRegisterMutation, useLogoutMutation} = userApiSlice
