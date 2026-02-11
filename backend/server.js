dca3b0a81179d1cf057c3ca722049bc3e200333f
sumaiyaAfroza <sumaiya.afroza.99@gmail.com>
2026-02-11 22:51:07 +0600

    vercel.json link add frontend vercel.josn

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000
 
 connectDB()
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' ? 'https://authentication-xetn.vercel.app' : 'http://localhost:5173',
+  origin: process.env.NODE_ENV === 'production' ? 'https://authentication-six-psi.vercel.app/' : 'http://localhost:5173',
   credentials: true
 }))
 app.use(express.json())
