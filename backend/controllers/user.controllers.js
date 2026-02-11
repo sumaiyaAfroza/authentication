7aa168e79e3aa92a845ab6165f9a1140a2106247
sumaiyaAfroza <sumaiya.afroza.99@gmail.com>
2026-02-11 22:34:35 +0600

    vercel.json link add frontend vercel.josn

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -6,16 +6,13 @@ import {generateToken} from "../utils/generate.token.js";
 export const registerUser = asyncHandler(async (req, res) => {
   const {name, email, password} = req.body
   const userExists = await User.findOne({email})
-
   if (userExists) {
     res.status(400)
     throw new Error('user already exists')
   }
-
   const user = await User.create({
     name, email, password
   })
-
   if (user) {
     generateToken(res, user._id)  // ✅ ঠিক - ছোট হাতের 'i'
     res.status(201).json({
