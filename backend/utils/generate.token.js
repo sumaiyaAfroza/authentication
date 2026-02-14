543251aff2d25bf8a9f216fc5015c6f8c7349557
sumaiyaAfroza <sumaiya.afroza.99@gmail.com>
2026-02-14 17:19:50 +0600

    generatetoken code modify

--- backend/utils/generate.token.js
+++ backend/utils/generate.token.js
@@ -1,3 +1,5 @@
+import jwt from "jsonwebtoken";
+
 export const generateToken = (res, userId) => {
   const token = jwt.sign({userId}, process.env.JWT_SECRET, {
     expiresIn: "30d"
@@ -5,17 +7,14 @@ export const generateToken = (res, userId) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    secure: process.env.NODE_ENV === 'production', // ✅ production এ true
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // ✅ cross-site এর জন্য 'none'
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    path: '/',
-    domain: process.env.NODE_ENV === 'production' ? '.vercel.app' : undefined
+    path: '/'
   })
 
   return token
 }
-
-
 
 
 
