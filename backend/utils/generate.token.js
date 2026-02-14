1a66cd7368506563e5339728b0bc7e12ffa7b413
sumaiyaAfroza <sumaiya.afroza.99@gmail.com>
2026-02-14 17:24:49 +0600

    updateprofile code modify for confirm password

--- backend/utils/generate.token.js
+++ backend/utils/generate.token.js
@@ -15,22 +15,3 @@ export const generateToken = (res, userId) => {
 
   return token
 }
-
-
-
-// import jwt from "jsonwebtoken";
-//
-// export const generateToken = ( res, userId) => {
-//   const token = jwt.sign({userId}, process.env.JWT_SECRET , {
-//     expiresIn: "30d"
-//   } )
-//   res.cookie('jwt', token, {
-//     httpOnly: true,
-//      secure: true,
-//     sameSite: "none",
-//     maxAge: 30 * 24 * 60 * 60 * 1000,
-//     path: '/'
-//   })
-//
-//   return token
-// }
